98eeb2d4b2736821d3ae3dc1f8eb711a8da0eec3
Apaulav <50292334+Apaulav@users.noreply.github.com>
2019-05-10 21:38:49 +0200

    Update NVL-JS-P10b.js

--- NVL-JS-P10b.js
+++ NVL-JS-P10b.js
@@ -2,15 +2,12 @@ var par =[];
 var impar =[];
 for (var i =0; i < 50; i++){
 	var azar = Math.floor((Math.random() * 100) + 1);
-    //document.write(azar + " ");
     
     if (azar % 2 == 0){	
-    	//document.write("par");
         par.push(azar);
         }
     else{
         impar.push(azar);
-    	//document.write(impar, " ");
         }  
  }
 document.write("Pares ",par, "<BR>");
